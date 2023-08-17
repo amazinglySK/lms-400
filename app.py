9d638d74b86c92876361f6f50f9fc17c5e2f2e92
amazinglysk <shashwat07ktm@gmail.com>
2023-08-18 00:17:19 +0400

    Added a quick login UI to setup credentials

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QFileDialog, QMainWindow, QApplication, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QFileDialog, QLineEdit, QMainWindow, QApplication, QMessageBox, QVBoxLayout, QLabel, QPushButton
 import mysql.connector
 import webbrowser
 import sys
+import os
 
 from views import BookWindow
 from views import MemberWindow
@@ -13,21 +14,44 @@ from models import Books
 from models import Member
 
 
+
+
+def check_credentials(): 
+    try :
+        with open("cred.txt", 'r') as file : 
+            l = file.readlines()
+            [db, user, pwd] = l
+            if not l : 
+                print("Empty credentials")
+                raise Exception
+
+            conn = mysql.connector.connect(host = "localhost", user = user, password = pwd, database = db)
+            if not conn.is_connected() : 
+                print("Not connected")
+                raise Exception
+
+            return conn
+
+    except Exception as err : 
+        print(err)
+        return False
+
 class DashboardView(QMainWindow):
-    def __init__(self):
+    def __init__(self, connection):
+        # Database
+        self.conn = connection
+        self.book_model = Books(connection=self.conn)
+        self.member_model = Member(connection=self.conn)
+        
+        # UI
         super(DashboardView, self).__init__()
         uic.loadUi("./ui/Dashboard Window.ui", self)
         self.member_button = self.findChild(QPushButton, "members_button")
         self.books_button = self.findChild(QPushButton, "books_button")
         self.report_button = self.findChild(QPushButton, "ReportButton")
 
-        # Database
-        self.conn = mysql.connector.connect(
-            host="localhost", user="root", password="tiger", database="LMS"
-        )
-        self.book_model = Books(connection=self.conn)
-        self.member_model = Member(connection=self.conn)
 
+        # Connecting signals
         self.member_button.clicked.connect(self._redirect_member)
         self.books_button.clicked.connect(self._redirect_books)
         self.report_button.clicked.connect(self._download_report)
@@ -55,8 +79,37 @@ class DashboardView(QMainWindow):
         xl_writer.add_worksheet("Books", books, book_header)
         xl_writer.finish_task()
 
-class Window(QMainWindow):
+class LoginWindow(QMainWindow) : 
     def __init__(self):
+        super(LoginWindow, self).__init__()
+        uic.loadUi("./ui/Login Window.ui", self)
+
+        self.database = self.findChild(QLineEdit, "DbName")
+        self.username = self.findChild(QLineEdit, "DbUsername")
+        self.pwd = self.findChild(QLineEdit, "DbPassword")
+        self.login_btn = self.findChild(QPushButton, "LoginButton")
+
+        self.login_btn.clicked.connect(self._login)
+
+    def _login(self) : 
+        details = [self.database.text(), self.username.text(), self.pwd.text()]
+        with open('cred.txt', 'w') as file : 
+            file.write("\n".join(details))
+
+        if not check_credentials() :
+            msg = QMessageBox()
+            msg.setText("Invalid Credentials")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.setWindowTitle("Alert")
+            msg.show()
+            return
+
+        self.window = Window(check_credentials())
+        self.window.show()
+        self.close()
+
+class Window(QMainWindow):
+    def __init__(self, connection):
         super(Window, self).__init__()
         uic.loadUi("./ui/Main Window.ui", self)
 
@@ -66,14 +119,21 @@ class Window(QMainWindow):
         self.instruction_button = self.findChild(QPushButton, "Instructions")
         self.instruction_button.clicked.connect(lambda : webbrowser.open("https://github.com/amazinglysk/lms-400"))
 
+        self.conn = connection
+
+
     def _on_dashboard_click(self):
-        self.w = DashboardView()
+        self.w = DashboardView(self.conn)
         self.w.show()
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = Window()
-    window.show()
+    if not check_credentials() :
+        login = LoginWindow()
+        login.show()
+    else : 
+        main_window = Window(check_credentials())
+        main_window.show()
     r = app.exec()
     sys.exit(r)
